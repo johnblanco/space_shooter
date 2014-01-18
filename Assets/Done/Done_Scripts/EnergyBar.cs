3812e1f20f29ec8679a656beeb2d3de82d56159d
Juan Pablo Blanco <j.pabloblanco@gmail.com>
2014-01-18 16:51:38 -0200

    experimentando con un componente de health bar

---
+++ Assets/Done/Done_Scripts/EnergyBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnergyBar : MonoBehaviour {
+
+  public GUITexture full;
+  
+  void Start () {
+    
+	}
+	
+	void Update () {
+	  if(Input.GetKeyUp(KeyCode.Space)){
+      full.enabled = !full.enabled;
+    }
+	}
+
+
+  IEnumerator Half(){
+    yield return new WaitForSeconds(3);
+
+  }
+}
