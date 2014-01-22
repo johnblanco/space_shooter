73a69d64c7bccc0e9b75598ebadb26da61ced233
Juan Pablo Blanco <j.pabloblanco@gmail.com>
2014-01-22 20:35:56 -0200

    aaa

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+	public float speed;
+
+	void Start ()
+	{
+<<<<<<< HEAD:Assets/Done/Done_Scripts/Done_Mover.cs
+		print (this.tag);
+		rigidbody.velocity = transform.forward * speed;
+=======
+    rigidbody.velocity = transform.forward * speed;
+>>>>>>> refs/heads/feature/no_mas_done:Assets/Scripts/Mover.cs
+	}
+
+}
