87fbe7ee928dd62ac6be372de46c565fafc36716
johnblanco <j.pabloblanco@gmail.com>
2014-01-24 03:52:34 -0800

    Update LootBox.cs

--- Assets/Scripts/LootBox.cs
+++ Assets/Scripts/LootBox.cs
@@ -9,6 +9,8 @@ public class LootBox : MonoBehaviour {
   void OnTriggerEnter (Collider other){
     if (other.tag == "Player")
     {
+      //FIXME: esta saltando null reference exception, creo que podria inicializar el gameController desde el destroyByContact
+      // que fue el que spawneo la caja
       if(isHealth)
         gameController.RestoreHealth(34);
       else
