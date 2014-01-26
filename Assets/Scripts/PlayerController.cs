b4c9ea68e9c031b6706224d23a5e2ca4d0dff411
echinelli <emilianochinelli@gmail.com>
2014-01-26 04:57:55 -0200

    agrego tema para weapons, pero lo importante es que movi prefabs y audio adentro de resources, necesario para cargar recursos en runtime.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,48 +2,53 @@
 using System.Collections;
 
 [System.Serializable]
-public class Boundary 
+public class Boundary
 {
-	public float xMin, xMax, zMin, zMax;
+  public float xMin, xMax, zMin, zMax;
 }
 
 public class PlayerController : MonoBehaviour
 {
-	public float speed;
-	public float tilt;
-	public Boundary boundary;
+  public float speed;
+  public float tilt;
+  public Boundary boundary;
+  
+  public Transform shotSpawn;
+  public Weapon currentWeapon;
+  
+  public void Start()
+  {
+    SimpleGun weapon = new SimpleGun(shotSpawn);
+    weapon.fireRate = 0.5f;
+    
+    this.currentWeapon = weapon;
+    
+  }
+    
+  void Update()
+  {
+    if (Input.GetButton("Fire1"))
+    {
+      this.currentWeapon.Fire();
+      
+    }
+  }
 
-	public GameObject shot;
-	public Transform shotSpawn;
-	public float fireRate;
-	 
-	private float nextFire;
-	
-	void Update ()
-	{
-		if (Input.GetButton("Fire1") && Time.time > nextFire) 
-		{
-			nextFire = Time.time + fireRate;
-			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
-			audio.Play ();
-		}
-	}
+  void FixedUpdate()
+  {
+    float moveHorizontal = Input.GetAxis("Horizontal");
+    float moveVertical = Input.GetAxis("Vertical");
 
-	void FixedUpdate ()
-	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		rigidbody.velocity = movement * speed;
-		
-		rigidbody.position = new Vector3
-		(
-			Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax), 
-			0.0f, 
-			Mathf.Clamp (rigidbody.position.z, boundary.zMin, boundary.zMax)
-		);
-		
-		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
-	}
+    Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+    rigidbody.velocity = movement * speed;
+        
+    rigidbody.position = new Vector3
+        (
+            Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax), 
+            0.0f, 
+            Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
+    );
+        
+    rigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+  }
 }
