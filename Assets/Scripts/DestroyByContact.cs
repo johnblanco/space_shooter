6e6a8d9259861fceb0b0984a3f2186a8f711d4fa
echinelli <emilianochinelli@gmail.com>
2014-02-03 15:06:25 -0200

    Revert "re organizacion scripts"
    This reverts commit ded3c576378496d9891c194f15fa115967019d81.

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+  public GameObject explosion;
+  public int scoreValue;
+  private GameController gameController;
+  public GameObject healthBox;
+  public GameObject weaponBox;
+
+  //los components que tienen attached este script son: enemigos, asteroides y bala de enemigo
+
+  void Start()
+  {
+    GameObject gameControllerObject = GameObject.FindGameObjectWithTag("GameController");
+    if (gameControllerObject != null)
+      {
+        gameController = gameControllerObject.GetComponent <GameController>();
+      }
+      if (gameController == null)
+        {
+          Debug.Log("Cannot find 'GameController' script");
+        }
+  }
+
+      void OnTriggerEnter(Collider other)
+      {
+        if (other.tag == "Boundary" || other.tag == "Enemy" || other.tag == "LootBox" || other.tag == "Bullet")
+          {
+            return;
+          }
+
+          if (explosion != null)
+            {
+              Instantiate(explosion, transform.position, transform.rotation);
+            }
+
+            if (other.tag == "Player")
+              {
+                gameController.HitPlayer(34);
+
+              } else
+                {
+                  Destroy(other.gameObject);
+                  MaybeGiveLoot();
+                }
+        
+                gameController.AddScore(scoreValue);
+        
+               
+                Destroy(gameObject);
+      }
+
+              void MaybeGiveLoot()
+              {
+                if (healthBox != null && weaponBox != null)
+                  {
+
+                    bool hasToSpawn = true;
+                    if (hasToSpawn)
+                      {
+                        bool isHealth = Random.Range(0, 100) % 2 == 0;
+                        if (isHealth)
+                          Instantiate(healthBox, transform.position, transform.rotation);
+                        else
+                          Instantiate(weaponBox, transform.position, transform.rotation);     
+                      }
+            
+                  }
+        
+              }
+}
