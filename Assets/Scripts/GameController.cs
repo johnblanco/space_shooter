48b7e351c5d20941a4275b12a28f3ffce99ef4a2
Juan Pablo Blanco <j.pabloblanco@gmail.com>
2014-02-13 20:53:58 -0200

    avanzando con el juego cooperativo

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,6 +7,7 @@ public class GameController : MonoBehaviour
   public GameObject playerExplosion;
   public GameObject player;
   public GameObject player2;
+  public GameObject energyBar2Wrapper;
   public GameObject[] hazards;
   public Vector3 spawnValues;
   public int hazardCount;
@@ -17,16 +18,18 @@ public class GameController : MonoBehaviour
   public GUIText restartText;
   public GUIText gameOverText;
   public EnergyBar energyBar;
+  public EnergyBar energyBar2;
   public int playerHealth;
+  public int playerHealth2;
   private bool gameOver;
   private bool restart;
   private int score;
   private TwoPlayerFlag twoPlayerFlag;
   private bool twoPlayerGame;
-    
+
   void Start()
   {
-    
+
     gameOver = false;
     restart = false;
     twoPlayerGame = false;
@@ -41,101 +44,115 @@ public class GameController : MonoBehaviour
 
     }
     player2.SetActive(twoPlayerGame);
+    energyBar2Wrapper.SetActive(twoPlayerGame);
+
   }
-    
+
   void Update()
   {
     if (restart)
+    {
+      if (Input.GetKeyDown(KeyCode.R))
       {
-        if (Input.GetKeyDown(KeyCode.R))
-          {
-            Application.LoadLevel(Application.loadedLevel);
-          }
+        Application.LoadLevel(Application.loadedLevel);
       }
+    }
   }
-    
-      IEnumerator SpawnWaves()
+
+  IEnumerator SpawnWaves()
+  {
+    yield return new WaitForSeconds(startWait);
+    while (true)
+    {
+      for (int i = 0; i < hazardCount; i++)
       {
-        yield return new WaitForSeconds(startWait);
-        while (true)
-          {
-            for (int i = 0; i < hazardCount; i++)
-              {
-                GameObject hazard = hazards [Random.Range(0, hazards.Length)];
-                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-                Quaternion spawnRotation = Quaternion.identity;
-                Instantiate(hazard, spawnPosition, spawnRotation);
-                yield return new WaitForSeconds(spawnWait);
-              }
-              yield return new WaitForSeconds(waveWait);
-              
-              if (gameOver)
-                {
-                  restartText.text = "Press 'R' for Restart";
-                  restart = true;
-                  break;
-                }
-          }
+        GameObject hazard = hazards [Random.Range(0, hazards.Length)];
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+        Quaternion spawnRotation = Quaternion.identity;
+        Instantiate(hazard, spawnPosition, spawnRotation);
+        yield return new WaitForSeconds(spawnWait);
       }
-    
-            public void AddScore(int newScoreValue)
-            {
-              score += newScoreValue;
-              UpdateScore();
-            }
-    
-            void UpdateScore()
-            {
-              scoreText.text = "Score: " + score;
-            }
-    
-            public void GameOver()
-            {
-              gameOverText.text = "Game Over!";
-              gameOver = true;
-            }
+      yield return new WaitForSeconds(waveWait);
 
-            public void RestoreHealth(int healthPoints)
-            {
-              playerHealth += healthPoints;
-              energyBar.health = Mathf.Clamp(playerHealth, healthPoints, 100);
-        
-            }
-    
-            public void HitPlayer(int hitPoints)
-            {
-              playerHealth -= hitPoints;
-              energyBar.health = playerHealth;
-              if (playerHealth <= 0)
-                {
-                  Destroy(player);
-                  Instantiate(playerExplosion, player.transform.position, player.transform.rotation);
-                  GameOver();
-                }
-            }
+      if (gameOver)
+      {
+        restartText.text = "Press 'R' for Restart";
+        restart = true;
+        break;
+      }
+    }
+  }
 
-              public void GiveWeapon()
-              {
-              
-                PlayerController pController = player.GetComponent<PlayerController>();
-                
-                if (pController.currentWeapon is SimpleGun)
-                  { 
-                    pController.currentWeapon = new TripleGun(pController.shotSpawn);
-                    return;
-                  }
-                
-                  if (pController.currentWeapon is TripleGun)
-                    {
-                      pController.currentWeapon = new GuidedMissile(pController.shotSpawn);
-                      return;
-                    }
-                
-                    if (pController.currentWeapon is GuidedMissile)
-                      {
-                        pController.currentWeapon = new SimpleGun(pController.shotSpawn);
-                        return;
-                      }
-                
-              }
+  public void AddScore(int newScoreValue)
+  {
+    score += newScoreValue;
+    UpdateScore();
+  }
+
+  void UpdateScore()
+  {
+    scoreText.text = "Score: " + score;
+  }
+
+  public void GameOver()
+  {
+    gameOverText.text = "Game Over!";
+    gameOver = true;
+  }
+
+  public void RestoreHealth(int healthPoints)
+  {
+    playerHealth = Mathf.Clamp(playerHealth + healthPoints, 0, 100);
+    energyBar.health = playerHealth;
+  }
+
+  public void HitPlayer(int hitPoints)
+  {
+    playerHealth = Mathf.Clamp(playerHealth-hitPoints,0,100);
+    energyBar.health = playerHealth;
+    if (playerHealth == 0)
+    {
+      Destroy(player);
+      Instantiate(playerExplosion, player.transform.position, player.transform.rotation);
+      if(playerHealth2 <= 0 || !twoPlayerGame)
+        GameOver();
+    }
+  }
+  public void HitPlayer2(int hitPoints)
+  {
+    playerHealth2 = Mathf.Clamp(playerHealth2-hitPoints,0,100);
+    energyBar2.health = playerHealth2;
+    if (playerHealth2 <= 0)
+    {
+      Destroy(player2);
+      Instantiate(playerExplosion, player2.transform.position, player2.transform.rotation);
+      if(playerHealth <= 0 )
+        GameOver();
+    }
+  }
+
+  public void GiveWeapon()
+  {
+
+    PlayerController pController = player.GetComponent<PlayerController>();
+
+    if (pController.currentWeapon is SimpleGun)
+    { 
+      pController.currentWeapon = new TripleGun(pController.shotSpawn);
+      return;
+    }
+
+    if (pController.currentWeapon is TripleGun)
+    {
+      pController.currentWeapon = new GuidedMissile(pController.shotSpawn);
+      return;
+    }
+
+    if (pController.currentWeapon is GuidedMissile)
+    {
+      pController.currentWeapon = new SimpleGun(pController.shotSpawn);
+      return;
+    }
+
+  }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,6 +15,7 @@ public class PlayerController : MonoBehaviour
   
   public GameObject shotSpawn;
   public Weapon currentWeapon;
+  public bool isPlayer2;
   
   public void Start()
   {
@@ -27,29 +28,38 @@ public class PlayerController : MonoBehaviour
     
   void Update()
   {
-    if (Input.GetButton("Fire1"))
-      {
-        
-        this.currentWeapon.Fire();
-      
-      }
+    if (Input.GetButton("Fire1") && !isPlayer2){        
+      this.currentWeapon.Fire();
+    }
+
+    if (Input.GetButton("Fire2") && isPlayer2){
+      this.currentWeapon.Fire();
+    }
   }
 
-    void FixedUpdate()
-    {
-      float moveHorizontal = Input.GetAxis("Horizontal");
-      float moveVertical = Input.GetAxis("Vertical");
+  void FixedUpdate()
+  {
+    float moveHorizontal;
+    float moveVertical;
 
-      Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-      rigidbody.velocity = movement * speed;
-        
-      rigidbody.position = new Vector3
-        (
-            Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax), 
-            0.0f, 
-            Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
-      );
-        
-      rigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+    if(!isPlayer2){
+      moveHorizontal = Input.GetAxis("Horizontal");
+      moveVertical = Input.GetAxis("Vertical");
+    }else{
+      moveHorizontal = Input.GetAxis("Player2Horizontal");
+      moveVertical = Input.GetAxis("Player2Vertical");
     }
+
+    Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+    rigidbody.velocity = movement * speed;
+      
+    rigidbody.position = new Vector3
+      (
+        Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax), 
+        0.0f, 
+        Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
+    );
+      
+    rigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+  }
 }
