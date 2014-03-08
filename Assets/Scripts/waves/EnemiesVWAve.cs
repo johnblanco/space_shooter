7bf1878dfd0c7aead597bc5ed04d7414418a9508
echinelli <emilianochinelli@gmail.com>
2014-03-08 11:48:49 -0200

    scripts enemies waves

---
+++ Assets/Scripts/waves/EnemiesVWAve.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemiesVWAve : Wave
+{
+
+    public GameObject[] enemies;
+
+    void Start()
+    {
+        spawnValues = new Vector3(0f, 0f, 16f);
+    }
+
+    public override void SpawnObject(int currentIndex)
+    {
+        
+        GameObject enemy = enemies [currentIndex % enemies.Length];
+        Vector3 spawnPosition;
+        GameObject instantiated;
+//        if (currentIndex % 2 == 0)
+//        {// spawn right
+        if (currentIndex == 0)
+        { 
+            spawnPosition = spawnValues;
+            instantiated = GameObject.Instantiate(enemy, spawnPosition, Quaternion.identity) as GameObject;
+            GameObject.DestroyImmediate(instantiated.GetComponent<EvasiveManeuver>(), true);
+        
+        } else
+        {
+            spawnPosition = new Vector3(spawnValues.x + 4f, spawnValues.y, spawnValues.z);
+            instantiated = GameObject.Instantiate(enemy, spawnPosition, Quaternion.identity) as GameObject;
+            GameObject.DestroyImmediate(instantiated.GetComponent<EvasiveManeuver>(), true);
+                
+            spawnPosition = new Vector3(spawnValues.x - 4f, spawnValues.y, spawnValues.z);
+            instantiated = GameObject.Instantiate(enemy, spawnPosition, Quaternion.identity) as GameObject;
+            GameObject.DestroyImmediate(instantiated.GetComponent<EvasiveManeuver>(), true);
+        
+        }
+            
+//        } else
+//        { // spawn left
+//            spawnPosition = new Vector3(spawnValues.x - 4f, spawnValues.y, spawnValues.z);
+//        }
+//        GameObject instantiated = GameObject.Instantiate(enemy, spawnPosition, Quaternion.identity) as GameObject;
+//        GameObject.DestroyImmediate(instantiated.GetComponent<EvasiveManeuver>(), true);
+//        
+    }
+ 
+}
---
+++ Assets/Scripts/waves/EnemiesWave.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemiesWave : Wave
+{
+    public GameObject[] enemies;
+  
+    void Start()
+    {
+        spawnValues = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>().spawnValues;
+    }
+	
+    public override void SpawnObject(int currentIndex)
+    { 
+        GameObject enemy = enemies [currentIndex % enemies.Length];
+        Vector3 spawnPosition = new Vector3(this.WrapPosition(-spawnValues.x, spawnValues.x, currentIndex), spawnValues.y, spawnValues.z);
+    
+        GameObject.Instantiate(enemy, spawnPosition, Quaternion.identity);
+    }
+}
