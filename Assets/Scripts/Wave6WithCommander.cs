b749486d15967fc49abd3b409570356e5ba6d50e
Juan Pablo Blanco <j.pabloblanco@gmail.com>
2014-03-09 17:47:45 -0300

    ahora con 2 waves!

---
+++ Assets/Scripts/Wave6WithCommander.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wave6WithCommander : Wave {
+  public GameObject enemy;
+  public void Start(){
+    spawnValues = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>().spawnValues;
+  }
+
+  public override void SpawnObject(int currentIeration){
+    Vector3 spawnPosition;
+    spawnPosition = new Vector3(-2,0,18);
+    Instantiate(enemy, spawnPosition, Quaternion.identity);
+  }
+} 
