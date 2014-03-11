4e16710ba90ab36b3c3054f46a6be0783481ea48
echinelli <emilianochinelli@gmail.com>
2014-03-11 20:09:06 -0300

    nueva ola, la que van en fila :P

---
+++ Assets/Scripts/movers/RedEnemyMoverPathFollower.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedEnemyMoverPathFollower : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("RightToLeft"), "time", 5, "easetype", iTween.EaseType.easeInOutSine,
+        "orienttopath", true));
+    }
+    
+}
---
+++ Assets/Scripts/waves/EnemyVMovingWave.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyVMovingWave : Wave
+{
+
+    public GameObject movingEnemy;
+    
+    void Start()
+    {
+    }
+    
+    public override void SpawnObject(int currentIndex)
+    { 
+        // spawn left
+        GameObject rightToLeftEnemy = Instantiate(movingEnemy, base.spawnValues, Quaternion.identity) as GameObject;
+        
+        iTween.MoveTo(rightToLeftEnemy, iTween.Hash("path", iTweenPath.GetPath("RightToLeft"), "time", 5, "easetype", iTween.EaseType.easeInOutSine,
+                                              "orienttopath", true));
+        // spawn right
+        GameObject leftToRightEnemy = Instantiate(movingEnemy, base.spawnValues, Quaternion.identity) as GameObject;
+        
+        iTween.MoveTo(leftToRightEnemy, iTween.Hash("path", iTweenPath.GetPath("LeftToRight"), "time", 5, "easetype", iTween.EaseType.easeInOutSine,
+                                           "orienttopath", true));
+    }
+    
+}
