369571962a20306bc3eb5d31fecaa67e268ad821
echinelli <emilianochinelli@gmail.com>
2014-03-12 01:57:51 -0300

    nueva ola de enemigos moviendose en "S"

---
+++ Assets/Scripts/waves/EnemiesSWave.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemiesSWave : Wave
+{
+    public GameObject enemy;
+
+    void Start()
+    {
+    
+    }
+    
+    public override void SpawnObject(int currentIndex)
+    {
+        GameObject instantiated = Instantiate(enemy, new Vector3(-6, 0, 24), Quaternion.identity) as GameObject;
+        iTween.MoveTo(instantiated, iTween.Hash("path", iTweenPath.GetPath("SLeftToRight"), "orienttopath", true, "time", 5,
+         "easetype", iTween.EaseType.easeInOutSine));
+    }
+}
