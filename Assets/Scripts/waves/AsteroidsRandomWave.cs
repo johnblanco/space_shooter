d8bdc74a083a96addf07e2fb144d78f539f28276
echinelli <emilianochinelli@gmail.com>
2014-03-11 22:26:33 -0300

    asteroids random wave + AsteroidsWaveManager

---
+++ Assets/Scripts/waves/AsteroidsRandomWave.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidsRandomWave : Wave
+{
+    public GameObject[] asteroids;
+
+    void Start()
+    {
+	
+    }
+	
+    public override void SpawnObject(int currentIndex)
+    {
+        GameObject original = asteroids [currentIndex % asteroids.Length];
+        
+        GameObject instantiated = Instantiate(original, new Vector3(Random.Range(-7, 7), 0, 16), Quaternion.identity) as GameObject;
+        instantiated.GetComponent<ZMover>().speed = Random.Range(-4, -7);
+    }
+}
