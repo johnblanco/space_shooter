c09bf9e06d04eae0bfb985917c5230a82fe51dc4
echinelli <emilianochinelli@gmail.com>
2014-03-11 21:19:18 -0300

    ola cinturón de asteroides convexo

---
+++ Assets/Scripts/waves/AsteroidsConvexBeltWave.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidsConvexBeltWave : Wave
+{
+    public GameObject[] asteroids;
+    
+    public void Start()
+    {
+    
+    }
+    
+    public override void SpawnObject(int currentIndex)
+    {
+        GameObject asteroid = asteroids [currentIndex % asteroids.Length];
+        
+        if (currentIndex == 0)
+        {
+            Instantiate(asteroid, new Vector3(-7, 0, 16), Quaternion.identity);
+            Instantiate(asteroid, new Vector3(7, 0, 16), Quaternion.identity);
+        } else if (currentIndex == 1)
+        {
+            Instantiate(asteroid, new Vector3(-5, 0, 15.6f), Quaternion.identity);
+            Instantiate(asteroid, new Vector3(5, 0, 15.6f), Quaternion.identity);
+        } else if (currentIndex == 2)
+        {
+            Instantiate(asteroid, new Vector3(-3, 0, 15.3f), Quaternion.identity);
+            Instantiate(asteroid, new Vector3(3, 0, 15.3f), Quaternion.identity);
+        } else if (currentIndex == 3)
+        {
+            Instantiate(asteroid, new Vector3(-1, 0, 15), Quaternion.identity);
+            Instantiate(asteroid, new Vector3(1, 0, 15), Quaternion.identity);
+        } else if (currentIndex == 4)
+        {
+            // Instantiate(asteroid, new Vector3(0, 0, 16), Quaternion.identity);
+            //Instantiate(asteroids, new Vector3(5, 0, 16), Quaternion.identity);
+        }
+    }
+    
+}
